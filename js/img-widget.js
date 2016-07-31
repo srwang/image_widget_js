b157ac9438604ab7fb3f51ffcce5f816a9e13bc0
Sharon Wag <sharon.ran.wang@gmail.com>
2016-07-30 20:39:25 -0400

    resized and added add/delete

--- js/img-widget.js
+++ js/img-widget.js
@@ -1,38 +1,45 @@
 angular.module('imgWidget', [])
-  .controller('ImageGalleryController', function() {
-    var gallery = this;
-    gallery.images = [
+  .controller('ImageGalleryController', ['$scope', function($scope) {
+    $scope.images = [
     	{
-    		id: 0,
 	    	title: 'Myself, kitten. Striving for cat.', 
 	    	caption: 'Dream about hunting birds. Bathe private parts with tongue then lick owner\'s face pooping rainbow while flying in a toasted bread costume in space love to play with owner\'s hair tie flop over, and loves cheeseburgers. Poop in litter box, scratch the walls kitty scratches couch bad kitty. Mark territory. Favor packaging over toy attack the dog then pretend like nothing happened stare at ceiling light yet when in doubt, wash cough furball wake up human for food at 4am. Stare at ceiling hopped up on catnip, or hopped up on catnip when in doubt, wash but brown cats with pink ears bleghbleghvomit my furball really tie the room together but i like big cats and i can not lie.', 
 	    	url: 'http://placekitten.com/500/800'
     	},
     	{
-    		id: 1,
     		title: 'Escape',
-    		caption: 'Not albino',
+    		caption: 'Still not albino',
     		url: 'http://placekitten.com/800/500'
     	},
     	{
-    		id: 2,
     		title: 'Iron Throne',
     		caption: 'Human give me attention meow fall over dead (not really but gets sypathy) so leave dead animals as gifts meow for need to chase tail, yet sleep on dog bed, force dog to sleep on floor.',
     		url: 'http://placekitten.com/800/800'
     	}
     ];
 
-    //add and delete image functionality
-    //create a wrapper for the widget
-    //responsive
+    $scope.addImage = function() {
+    	$scope.images.push({	
+    		title:$scope.imgTitle, 
+    		caption:$scope.imgCaption, 
+    		url:$scope.imgUrl
+		});
+    	$scope.imgTitle= '';
+    	$scope.imgCaption = '';
+    	$scope.imgUrl = '';
 
-    gallery.addImage = function() {
-    	gallery.images.push({title:gallery.addTitle, caption:gallery.addCaption, url:gallery.addUrl});
-    	gallery.addTitle = '';
-    	gallery.addImages = '';
-    	gallery.addUrl = '';
+    	setTimeout(function(){
+	    	if($scope.images.length===1) {
+	    		$('.img-container#0').show(); 
+            }
+    	}, 500);
     }
 
-  });
+    $scope.removeImage = function(imageId) {
+    	$('.img-container#' + imageId).find('.next-btn').trigger('click');
+    	$scope.images.splice(imageId, 1);
+    }
+
+  }]);
 
 
--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
 $(document).ready(function(){
 
-	$('.img-container:first-child').css({'display':'block'});
 
-	$('.next-btn').click(function(){
-		changeImage('next', $(this));
-	});
-	$('.last-btn').click(function(){
-		changeImage('last', $(this));
-	})
+$('.img-container#0').show();
 
-	function changeImage(direction, btn) {
-		var imgContainer = btn.parents()[2];
-		$(imgContainer).hide();
+//show images
+$('body').on('click', '.next-btn', function(){
+	changeImage('next', $(this));
+});
+$('body').on('click', '.last-btn', function(){
+	changeImage('last', $(this));
+});
 
-		var index = parseInt($(imgContainer).attr('id'));
+function changeImage(direction, btn) {
+	var imgContainer = btn.parents()[2];
+	$(imgContainer).hide();
 
-		if (direction==='next' && index <($('.img-container').length-1)) {
-			index +=1;
-		} else if (direction==='next') {
-			index = 0;
-		} else if (direction==='last' && index > 0) {
-			index -=1;
-		} else if (direction==='last') {
-			index = $('.img-container').length-1;
-		};
+	var index = parseInt($(imgContainer).attr('id'));
 
-		$('.img-container#' + index).show();
-	}
+	if (direction==='next' && index <($('.img-container').length-1)) {
+		index +=1;
+	} else if (direction==='next') {
+		index = 0;
+	} else if (direction==='last' && index > 0) {
+		index -=1;
+	} else if (direction==='last') {
+		index = $('.img-container').length-1;
+	};
 
+	$('.img-container#' + index).fadeIn(200);
+}
 
+//resize img container
+$('.img-container').each(function(){
+	var imageId = $(this).attr('id'),
+		hiddenImg = document.getElementsByClassName('hidden-img')[imageId],
+		proportion = hiddenImg.naturalWidth / hiddenImg.naturalHeight,
+		newWidth = $(this).height() * proportion;
+
+	$(this).css({'max-width':newWidth});
+});
 
 });
